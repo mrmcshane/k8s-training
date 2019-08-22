20329ac88c70faf5f537fb6d9010c8b870d9c22b
Andrew Mcshane <andrew.mcshane@valtech.com>
2019-08-22 16:31:07 +0100

    updating python application in 03 to be pretty

---
+++ 03-stateful-application/containers/python/app/test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import MySQLdb
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def databasetest():
+
+    db_output = "OFFLINE"
+    try:
+        db = MySQLdb.connect(host="mariadb-clusterip.default.svc.cluster.local",
+                            user="test_user", 
+                            passwd="test_pass",
+                            db="test_db") 
+        if db:
+            db_output = "ONLINE"
+    except:
+        pass
+
+    return render_template('index.html', variable=db_output)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
